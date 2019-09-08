53f4b85f8395a30ca049eca947648595a43aa613
Barry Gitarts <bgitarts@gmail.com>
2019-09-08 06:58:42 -0400

    add send eth and email

--- app/components/SendTx.jsx
+++ app/components/SendTx.jsx
@@ -5,13 +5,24 @@ import classnames from 'classnames'
 import { Formik } from 'formik'
 import useStyles from '../styles/dapp'
 
+const subject = 'I just sent you some crypto currency'
+const body = amount => `I just sent you ${amount} ETH to help you get started with Ethereum. You can access your Ethereum wallet just using your gmail account. Go to http://tor.us to start using it!`
 function SendTransaction({ contact }) {
   const classes = useStyles()
   return (
     <Formik
       initialValues={{ amount: '' }}
-      onSubmit={(values, { setSubmitting }) => {
+      onSubmit={async (values, { setSubmitting }) => {
         const amount = web3.utils.toWei(values.amount)
+        const from = await web3.eth.getCoinbase()
+        const toSend = web3.eth.sendTransaction({
+          from,
+          to: contact.address,
+          value: amount
+        }).then(res => {
+          console.log({res})
+          window.location.href = `mailto:${contact.email}?subject=${subject}&body=${body(values.amount)}`;
+        }).catch(console.log)
         console.log({amount, contact})
       }}
     >
@@ -25,19 +36,18 @@ function SendTransaction({ contact }) {
       isSubmitting,
     }) => (
       <form className={classnames(classes.root, classes.fullWidth)} onSubmit={handleSubmit}>
-      <TextField
-        id="outlined-bare"
-        name="amount"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-        placeholder="Enter ETH amount to send"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values.amount}
-      />
-
-      <Button type="submit" className={classes.button}>Send ETH</Button>
+        <TextField
+          id="outlined-bare"
+          name="amount"
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+          placeholder="Enter ETH amount to send"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.amount}
+        />
+        <Button type="submit" className={classes.button} disabled={isSubmitting}>{isSubmitting ? `Sending your friend ${values.amount }ETH`: 'Send ETH'}</Button>
       </form>
     )}
     </Formik>
