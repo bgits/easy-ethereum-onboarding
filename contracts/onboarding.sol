f4563e680ccfb74fd90120c3ae9a9f7b7bc27c9b
Barry Gitarts <bgitarts@gmail.com>
2019-09-08 11:39:04 -0400

    add recover contract

--- contracts/onboarding.sol
+++ contracts/onboarding.sol
@@ -2,21 +2,38 @@ pragma solidity ^0.5.11;
 
 contract Onboarding {
 
-  event Deposit(address indexed receiver, uint256 _value);
+  struct DepositInfo {
+    address funder;
+    uint256 amount;
+  }
 
-  mapping(address => uint256) balances;
+  event Deposit(address indexed receiver, uint256 _value);
+  event Recover(address indexed receiver);
+
+  mapping(address => DepositInfo) balances;
 
   function deposit(address receiver, uint amount) payable public {
     require(msg.value == amount);
-    balances[receiver] += amount;
+    balances[receiver] = DepositInfo({
+      funder: msg.sender,
+      amount: amount
+    });
     emit Deposit(receiver, amount);
   }
 
   function withdraw() public {
-    msg.sender.transfer(balances[msg.sender]);
+    msg.sender.transfer(balances[msg.sender].amount);
   }
 
   function getBalance() public view returns (uint256) {
-    return balances[msg.sender];
+    return balances[msg.sender].amount;
+  }
+
+  function recover(address receiver) public {
+    DepositInfo memory info = balances[receiver];
+    require(msg.sender == info.funder);
+    msg.sender.transfer(info.amount);
+    emit Recover(receiver);
+
   }
 }
